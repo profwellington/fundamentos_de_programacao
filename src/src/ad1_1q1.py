7f537a2c10a6e10efe4ecea480c8b5db67ef36e9
sswellington <wellws@pm.me>
2021-09-13 23:25:34 -0300

    refactor: adicionado o pacote - math

--- src/src/ad1_1q1.py
+++ src/src/ad1_1q1.py
@@ -1,20 +1,23 @@
-# Programa - AD1.1Q1
-# Subprogramas
-# Principal
-PI = 3.1415
+#!/usr/bin/env python
+
+''' Programa - AD1.1Q1 '''
+
+import math
+
+
 linha = input()
 while linha != "":
     numero = int(linha)
-    if numero%2 != 0:
+    
+    if ((numero % 2) != 0):
         raio = numero
-        area = PI*raio**2
-        perimetro = 2*PI*raio
+        area = math.pi * (raio ** 2)
+        perimetro = 2 * math.pi * raio
         print("Área e Perímetro do Círculo de Raio %d são: %.2f e %.2f"%(raio, area, perimetro))
     else:
         print("Divisores de %d são:"%numero, end="")
-        for divisor in range(1, numero+1):
-            if numero%divisor == 0:
+        for divisor in range(1, numero + 1):
+            if numero % divisor == 0:
                 print("", divisor, end="")
         print()
     linha = input()
-
